680e4a5a71aebf37c20e5ace141be8566541abef
Luciano-project <luhugo.lhcs@gmail.com>
2024-04-27 14:18:22 +0100

    refactor: the notifications was rewrited

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ while True:
 
     if opcao == '1':
         valor_saque = int(input("Digite o valor a ser sacado: "))
-        if valor_saque <= 0: print("Valor inválido!")
-        elif valor_saque > saldo: print("Saldo insuficiente!")
-        elif valor_saque > saque_maximo_dia: print("Valor de saque excede o limite diário!")
-        elif limite_vezes_saque == 0: print("Limite de saques diários atingido!")
+        if valor_saque <= 0: print("Operação Falhou: Valor inválido!")
+        elif valor_saque > saldo: print("Operação Falhou: Saldo insuficiente!")
+        elif valor_saque > saque_maximo_dia: print("Operação Falhou: Valor de saque excede o limite diário!")
+        elif limite_vezes_saque == 0: print("Operação Falhou: Limite de saques diários atingido!")
         else:
             saldo -= valor_saque
             extrato.append(f"Saque: R$ {valor_saque}")
@@ -48,7 +48,7 @@ while True:
                 
             elif valor_depositado <= 0:
                 os.system('cls')
-                print("Valor por depósito precisa ser positivo!")
+                print("Operação Falhou: Valor por depósito precisa ser positivo!")
                 break
 
 
